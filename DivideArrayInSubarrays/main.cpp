680879968edd6da7a2dd83e441dc0a06551f5f3f
Christopher <chris.needhame277@gmail.com>
2026-02-01 21:57:01 +0000

    DivideArray

---
+++ DivideArrayInSubarrays/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+    int DivideArray(vector<int> nums) {
+        int fixedValue = nums[0];
+        vector<int> remainder(nums.begin() + 1, nums.end());
+        sort(remainder.begin(), remainder.end());
+        return fixedValue + remainder[0] + remainder[1];
+    };
+};
+
+int main() {
+    Solution sol;
+    vector<int> myNumbers = {10, 5, 5, 8, 3};
+
+    int result = sol.DivideArray(myNumbers);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
