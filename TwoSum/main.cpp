d9faad64cd4b05c85913042177f6d0decc924dd2
Christopher <chris.needhame277@gmail.com>
2026-02-01 23:06:36 +0000

    Implement two-sum solution

---
+++ TwoSum/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+    vector<int> TwoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.contains(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main() {
+    Solution solution;
+    int target = 17;
+    vector<int> nums = {2,7,11,15};
+
+    vector<int> result = solution.TwoSum(nums, target);
+
+     if (!result.empty()) {
+         cout << result[0] << " " << result[1] << endl;
+     }
+    return 0;
+}
