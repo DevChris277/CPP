5608759270936d8dd9c00287d37fcd85b7a1e157
Christopher <chris.needhame277@gmail.com>
2026-02-04 21:42:17 +0000

    Add ValidAnagram

---
+++ ValidAnagram/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+class Solution {
+public:
+    static bool func(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char,int> map1, map2 = {};
+
+        for (int i = 0; i <= s.size(); i++) {
+            map1[s[i]] = 1 + map1[s[i]];
+            map2[t[i]] = 1 + map2[t[i]];
+        }
+
+        if (map1 != map2) return false;
+
+        return true;
+    }
+};
+
+int main() {
+    const string s = "anagram";
+    const string t = "nagaram";
+    bool result = Solution::func(s, t);
+
+    cout << result;
+
+    return 0;
+}
