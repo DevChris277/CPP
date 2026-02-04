b4f787105c58259ac96c3dce866326c3611fc485
Christopher <chris.needhame277@gmail.com>
2026-02-04 19:31:38 +0000

    Add ContainDup & ConcatArray

---
+++ ConcatenationOfArray/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+class Solution {
+public:
+    static vector<int> func(vector<int> nums) {
+        vector<int> ans = {};
+        for ( int j = 0; j < 2; j++) {
+            for (int i : nums) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    const vector<int> x = {1,542,1};
+    vector<int> result = Solution::func(x);
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
---
+++ ContainsDuplicates/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+    public:
+    static bool func(vector<int> nums) {
+        unordered_set<int> set = {};
+
+        for (int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }else {
+                set.insert(i);
+            }
+        }
+        return false;
+    }
+};
+
+
+int main() {
+    const vector<int> x = {1,2,5,6,1};
+    bool result = Solution::func(x);
+
+    cout << result;
+
+    return 0;
+}
